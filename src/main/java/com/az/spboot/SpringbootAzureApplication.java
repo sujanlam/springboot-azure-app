28a023628c686b94353e243fce4464ef8e850cce
Sujan Lam <sujann42@gmail.com>
2024-06-19 12:22:26 -0500

    first commit

---
+++ src/main/java/com/az/spboot/SpringbootAzureApplication.java
@@ -0,0 +1,21 @@
+package com.az.spboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootAzureApplication {
+
+	@GetMapping("/msg")
+	public String getMessage(){
+		return "This is the first part of the message!!!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAzureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/az/spboot/SpringbootAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.az.spboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
